da8c52b6fb838574b07b21ebaa3af5c202c9b650
Ajay Raj <ajay@airbears2-10-142-25-60.airbears2.1918.berkeley.edu>
2017-10-07 02:12:01 -0700

    cleaned up

--- entityExtractor.js
+++ entityExtractor.js
@@ -19,8 +19,11 @@ EntityExtractor.analyze = (text) => {
     .catch((err) => {
       console.error('ERROR:', err);
     });
-}
+  return entities
+};
 
 // Detects entities in the document
 
-console.log(EntityExtractor.analyze('After spending 40 years in slavery, he was freed in 1828 by order of President John Quincy Adams and Secretary of State Henry Clay after the Sultan of Morocco requested his release'))
+EntityExtractor.analyze('After spending 40 years in slavery, he was freed in 1828 by order of President John Quincy Adams and Secretary of State Henry Clay after the Sultan of Morocco requested his release').then((response) => {
+    console.log(response);
+});
