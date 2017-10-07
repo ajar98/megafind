5fc1e7a0f46fe90bdcdda6d6ab579abe1a83c08d
Kartik Kapur <kapurkartik1@gmail.com>
2017-10-07 04:39:15 -0700

    created sessionsclass for joining and creating sessions

---
+++ sessions.js
@@ -0,0 +1,36 @@
+const lecture = require('./lecture');
+
+var hash = {};
+    class sessions{
+    constructor(){
+    }
+    createSession(){
+        var numb = Math.floor(Math.random()*20);
+        console.log(numb);
+        while((numb.toString() in hash)){
+            numb = Math.floor(Math.random()*20);
+        }
+        hash[numb.toString()] = new lecture(numb);
+        return numb;
+        }
+
+
+    joinSession(key){
+        var strrep = key.toString();
+        if(strrep in hash){
+            return hash[strrep];
+        }
+        else{
+            return 'Error Invalid Entry Code.'
+        }
+    }
+    }
+
+
+module.exports = sessions;
+
+l = new sessions();
+var v = l.createSession();
+
+console.log(l.joinSession(v));
+console.log(l.joinSession(v+1));
