2efe425b0027f1a8025943ee89c7c3cb52d6000d
Ajay Raj <ajay@airbears2-10-142-25-60.airbears2.1918.berkeley.edu>
2017-10-07 05:30:31 -0700

    end of night 1 bois

---
+++ utils/entityextractor.js
@@ -0,0 +1,27 @@
+const Language = require('@google-cloud/language');2
+
+class Entity {
+    constructor () {
+      this.language = Language();
+    }
+
+    /* DOCUMENT must look like THIS:
+      {
+      'content' = text,
+      type = 'PLAIN_TEXT'
+    }
+    */
+    getEntities(doc) {
+      return this.language.analyzeEntities({ document: doc }).then((results) => {
+        const result = {}
+        const entities = results[0].entities;
+
+        entities.forEach((entity) => {
+          if (entity.metadata && entity.metadata.wikipedia_url) {
+            result[`${entity.name}`] = entity.metadata.wikipedia_url;
+          }
+        })
+        return result
+      });
+    }
+}
