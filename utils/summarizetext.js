0c4d8da602cb52084cb754f9edbaeff9070ef5cc
Kian Hooshmand <kianhooshmand@berkeley.edu>
2017-10-07 22:14:11 -0700

    Finished summarizer class

--- utils/summarizetext.js
+++ utils/summarizetext.js
@@ -1,65 +1,44 @@
+const request = require('request-promise');
+const winston = require('winston');
+const R = require('ramda');
+
 class Summarizer {
   constructor() {
-    this.options = {
-      method: 'POST',
-      url: 'http://api.intellexer.com/summarizeText',
-      qs: {
-        apikey: '0d175b94-7204-45cd-9231-87178dbdb22f',
-        summaryRestriction: '2',
-        returnedTopicsCount: '2',
-        loadConceptsTree: 'false',
-        loadNamedEntityTree: 'false',
-        usePercentRestriction: 'false',
-        conceptsRestriction: '7',
-        structure: 'general',
-        fullTextTrees: 'false',
-        textStreamLength: '1000',
-        wrapConcepts: 'true' },
-      headers:{ 'cache-control': 'no-cache' }
-    }
-  }
+    this.options = { method: 'POST',
+        url: 'http://api.intellexer.com/summarizeText',
+        qs:
+         { apikey: '0d175b94-7204-45cd-9231-87178dbdb22f',
+           summaryRestriction: '8',
+           returnedTopicsCount: '2',
+           loadConceptsTree: 'false',
+           loadNamedEntityTree: 'false',
+           usePercentRestriction: 'false',
+           conceptsRestriction: '7',
+           structure: 'general',
+           fullTextTrees: 'false',
+           textStreamLength: '1000',
+           wrapConcepts: 'true' },
+           headers:{ 'cache-control': 'no-cache' }}
+    this.summary = null;
+  };
 
-  summarizeText(text) {
+  summarizeText (text) {
     this.options.body = text;
+    return request(this.options).then((body) => {
+      body = JSON.parse(body);
+      let summary = "";
+      R.forEach((entry) => {
+        if (entry.text != null) {
+          summary += entry.text + " ";
+        }
+      }, body.items)
 
-    request.post('http://api.intellexer.com/summarizeText', {})
-
+      this.summary = summary;
+      return this.summary
+    })
   }
 }
 
 
-
-var newbod = null;
-var request = require("request");
-function summarizeText(txt){
-    var options = { method: 'POST',
-  url: 'http://api.intellexer.com/summarizeText',
-  qs:
-   { apikey: '0d175b94-7204-45cd-9231-87178dbdb22f',
-     summaryRestriction: '2',
-     returnedTopicsCount: '2',
-     loadConceptsTree: 'false',
-     loadNamedEntityTree: 'false',
-     usePercentRestriction: 'false',
-     conceptsRestriction: '7',
-     structure: 'general',
-     fullTextTrees: 'false',
-     textStreamLength: '1000',
-     wrapConcepts: 'true' },
-  headers:
-   { 'cache-control': 'no-cache' },
-  body: txt };
-  request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  newbod = JSON.parse(body);
-});
-  setTimeout(slowDown, txt.length/2);
-}
-function slowDown(){
-    var summary ="";
-    for(var i = 0 ; i < newbod["items"].length; i++){
-    summary+= newbod["items"][i]["text"];
-  }
-    console.log(summary);
-    return summary;
-}
+const s = new Summarizer();
+s.summarizeText(text).then((result) => console.log(result));
